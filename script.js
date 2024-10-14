67d3fa3d8b8550ce504f844c4b81739fd9a7753c
Nachiket <alphaonfield@gmail.com>
2024-10-14 13:34:09 +0530

    some minor updates

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navLinks = document.querySelector(".nav-links");
 
-// Add a click event listener to the hamburger icon
 hamburgerMenu.addEventListener("click", () => {
-  // Toggle the 'active' class to show/hide the navigation links
   navLinks.classList.toggle("active");
 });
